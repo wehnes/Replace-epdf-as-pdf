ddca2340bf69e50260069499d9ca22372e2b6e64
wehnes <14278110+wehnes@users.noreply.github.com>
2022-06-08 02:09:33 +0800

    Update Replace epdf as pdf.user.js

--- Replace epdf as pdf.user.js
+++ Replace epdf as pdf.user.js
@@ -20,21 +20,4 @@
         href = $(".coolBar__ctrl.pdf-download")[1].href.replace(/epdf/, "pdf");
         $(".coolBar__ctrl.pdf-download")[1].setAttribute("href",href)
         console.log($(".coolBar__ctrl.pdf-download")[1].href)});
-    //function replace(){
-    //if(place.length == 1){
-   //     $(".coolBar__ctrl.pdf-download")[1].href.replace(/epdf/, "pdf");
-    //}
-   // else{
-   //     console.log("Not Found");
-    //}
-   // }
-
-    //var href_old, href_new;
-    // href_old = document.querySelector("#article__content > div.fixedCoolBar > nav > div > div.coolBar__second.rlist > div.coolBar__section.coolBar--download.PdfLink.cloned > a").href;
-    //href_old = $(".coolBar__ctrl.pdf-download")[1].href;
-    //href_new = href_old.replace(/epdf/, "pdf");
-    //console.log(href_new);
-    // document.querySelector("#article__content > div.fixedCoolBar > nav > div > div.coolBar__second.rlist > div.coolBar__section.coolBar--download.PdfLink.cloned > a").setAttribute("href",href_new);
-    //$(".coolBar__ctrl.pdf-download")[1].attr("href",href_new);
-    // $(".coolBar__ctrl.pdf-download")[1].click(function(){alert("sb")});
 })();
